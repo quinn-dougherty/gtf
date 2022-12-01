193db1b32e2af49c28188cf151ddc6fc01f619c8
Quinn Dougherty <quinnd@riseup.net>
2022-12-01 18:58:09 -0500

    Debugging herc (3)

--- nix/herc.nix
+++ nix/herc.nix
@@ -1,22 +1,23 @@
 { self, withSystem, ... }:
-let hciSystem = "x86_64-linux";
+let
+  hciSystem = "x86_64-linux";
+  packages = self.packages.${hciSystem};
 in {
   herculesCI = withSystem hciSystem ({ config, effects, pkgs, inputs', ... }: {
     ciSystems = [ hciSystem ];
-    onPush = let packages = self.packages.${hciSystem};
-    in {
+    onPush = {
       checks.outputs = self.checks.${hciSystem}.lint;
       coq-game-theory.outputs = packages.coq-game-theory;
       nashwires.outputs = packages.nashwires;
       development.outputs = self.devShells.${hciSystem};
-      comms = {
-        outputs = { agent-signature = packages.agent-signature; };
-        effects = {
-          agent-signature = effects.mkEffect {
-            effectScript =
-              "putStateFile agent-signature.pdf ${packages.agent-signature}/agent-signature.pdf";
-          };
-        };
+      comms.outputs = { agent-signature = packages.agent-signature; };
+    };
+    effects = let packages = self.packages.${hciSystem};
+    in {
+      agent-signature = effects.mkEffect {
+        effectScript =
+          "putStateFile agent-signature.pdf ${packages.agent-signature}/agent-signature.pdf";
+
       };
     };
   });
