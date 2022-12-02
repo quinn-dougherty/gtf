be5a0f261c515a4843877ec8d8beb4a3d60542a8
Quinn Dougherty <quinnd@riseup.net>
2022-12-02 14:19:09 -0500

    Moving around the `branch` input (6)

--- nix/herc.nix
+++ nix/herc.nix
@@ -4,37 +4,35 @@ let
   packages = self.packages.${hciSystem};
   comms-documents = (import ./lib.nix { inherit lib; }).dirNames ./../comms;
 in {
-  herculesCI = withSystem hciSystem ({ hci-effects, ... }:
-    let
-      run-condition = branch: branch == "main";
-      documentEffectForAll = branch:
-        let
+  herculesCI = hercInputs:
+    withSystem hciSystem ({ hci-effects, ... }:
+      let
+        run-condition = hercInputs.branch == "main";
+        documentEffectForAll = let
           documentEffectFor = document: {
             name = document;
-            value = hci-effects.runIf (run-condition branch)
-              (hci-effects.mkEffect {
-                effectScript = "putStateFile ${document}.pdf ${
-                    packages.${document}
-                  }/${document}.pdf";
-              });
+            value = hci-effects.runIf run-condition (hci-effects.mkEffect {
+              effectScript = "putStateFile ${document}.pdf ${
+                  packages.${document}
+                }/${document}.pdf";
+            });
           };
         in documents: builtins.listToAttrs (map documentEffectFor documents);
-      soupaultDeploy = branch:
-        hci-effects.netlifyDeploy {
+        soupaultDeploy = hci-effects.netlifyDeploy {
           content = packages.soupault;
-          productionDeployment = run-condition branch;
+          productionDeployment = run-condition;
           secretName = "default-netlify";
           siteId = "b982a29f-501a-43ac-8702-3800e8e22cf5";
         };
-    in { branch, ... }: {
-      ciSystems = [ hciSystem ];
-      onPush = {
-        checks.outputs = self.checks.${hciSystem}.lint;
-        coq-game-theory.outputs = packages.coq-game-theory;
-        nashwires.outputs = packages.nashwires;
-        development.outputs = self.devShells.${hciSystem};
-        comms.outputs.effects = documentEffectForAll branch comms-documents;
-        soupault.outputs.effects = soupaultDeploy branch;
-      };
-    });
+      in {
+        ciSystems = [ hciSystem ];
+        onPush = {
+          checks.outputs = self.checks.${hciSystem}.lint;
+          coq-game-theory.outputs = packages.coq-game-theory;
+          nashwires.outputs = packages.nashwires;
+          development.outputs = self.devShells.${hciSystem};
+          comms.outputs.effects = documentEffectForAll comms-documents;
+          soupault.outputs.effects = soupaultDeploy;
+        };
+      });
 }
