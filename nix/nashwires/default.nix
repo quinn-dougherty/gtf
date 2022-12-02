fd8d355283601e14133406eec90c2bd110a7b99d
Quinn Dougherty <quinnd@riseup.net>
2022-12-02 16:16:04 -0500

    Advanced nix build of the rescript package: blocked on `mkdir` permissions in the testing libraries

--- nix/nashwires/default.nix
+++ nix/nashwires/default.nix
@@ -1,22 +1,28 @@
 { self, ... }: {
   perSystem = { config, self', inputs', pkgs, ... }:
     let
-      nodeDependencies =
-        (pkgs.callPackage ./npm.nix { inherit pkgs; }).nodeDependencies;
+      nodeDependencies = (pkgs.callPackage ./npm.nix {
+        inherit pkgs;
+        system = pkgs.system;
+        nodejs = pkgs.nodejs-14_x;
+      }).nodeDependencies;
     in {
       packages.nashwires = pkgs.stdenv.mkDerivation {
         name = "nashwires-compile-test";
         src = ./../../nashwires;
         buildInputs = [ pkgs.nodejs-14_x ];
-        buildPhase = ''
+        configurePhase = ''
           ln -s ${nodeDependencies}/lib/node_modules ./node_modules
           export PATH="${nodeDependencies}/bin:$PATH"
-
-          npm run build
+        '';
+        buildPhase = ''
+          npm run build:peggy
+          npm run build:rescript
           npm run test
         '';
         installPhase = ''
           mkdir -p $out
+          cp -r lib $out
         '';
       };
     };
