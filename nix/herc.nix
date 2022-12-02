a9555721e2c822d4406a5f37bdcd650a8eeb5272
Quinn Dougherty <quinnd@riseup.net>
2022-12-02 13:48:05 -0500

    Netlify up

--- nix/herc.nix
+++ nix/herc.nix
@@ -4,37 +4,37 @@ let
   packages = self.packages.${hciSystem};
   comms-documents = (import ./lib.nix { inherit lib; }).dirNames ./../comms;
 in {
-  herculesCI = withSystem hciSystem ({ hci-effects, ... }:
-    let
-      util = let
-        documentEffectFor = document: {
-          name = document;
-          value = hci-effects.mkEffect {
-            effectScript = "putStateFile ${document}.pdf ${
-                packages.${document}
-              }/${document}.pdf";
+  herculesCI = { branch }:
+    withSystem hciSystem ({ hci-effects, ... }:
+      let
+        util = let
+          documentEffectFor = document: {
+            name = document;
+            value = hci-effects.runIf (branch == "main") (hci-effects.mkEffect {
+              effectScript = "putStateFile ${document}.pdf ${
+                  packages.${document}
+                }/${document}.pdf";
+            });
+          };
+        in {
+          documentEffectForAll = documents:
+            builtins.listToAttrs (map documentEffectFor documents);
+          soupaultDeploy = hci-effects.netlifyDeploy {
+            content = packages.soupault;
+            productionDeployment = (branch == "main");
+            secretName = "default-netlify";
+            siteId = "b982a29f-501a-43ac-8702-3800e8e22cf5";
           };
         };
       in {
-        documentEffectForAll = documents:
-          builtins.listToAttrs (map documentEffectFor documents);
-        soupaultDeploy = branch:
-          hci-effects.netlifyDeploy {
-            content = packages.soupault;
-            productionDeployment = (branch == "main");
-            secretName = "netlify";
-            siteId = "9d17ad40-c2f8-4933-b7d8-bb0ac30f0907";
-          };
-      };
-    in {
-      ciSystems = [ hciSystem ];
-      onPush = {
-        checks.outputs = self.checks.${hciSystem}.lint;
-        coq-game-theory.outputs = packages.coq-game-theory;
-        nashwires.outputs = packages.nashwires;
-        development.outputs = self.devShells.${hciSystem};
-        comms.outputs.effects = util.documentEffectForAll comms-documents;
-        soupault.outputs.effects = util.soupaultDeploy;
-      };
-    });
+        ciSystems = [ hciSystem ];
+        onPush = {
+          checks.outputs = self.checks.${hciSystem}.lint;
+          coq-game-theory.outputs = packages.coq-game-theory;
+          nashwires.outputs = packages.nashwires;
+          development.outputs = self.devShells.${hciSystem};
+          comms.outputs.effects = util.documentEffectForAll comms-documents;
+          soupault.outputs.effects = util.soupaultDeploy;
+        };
+      });
 }
